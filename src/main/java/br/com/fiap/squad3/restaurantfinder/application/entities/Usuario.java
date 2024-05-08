5d80ed985dab0f299ed3e08c45cc5c33d155d44d
Barreto <v.barreto.and@gmail.com>
2024-05-08 00:46:46 -0300

    create domain entity

---
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/application/entities/Usuario.java
@@ -0,0 +1,94 @@
+package br.com.fiap.squad3.restaurantfinder.application.entities;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private Long id;
+    private String cpf;
+    private String nome;
+    private String ddd;
+    private String telefone;
+    private String email;
+    private LocalDate dataCadastro;
+
+    public Usuario(Long id, String cpf, String nome, String ddd, String telefone, String email, LocalDate dataCadastro) {
+        this.id = id;
+        this.setCpf(cpf);
+        this.setNome(nome);
+        this.setDdd(ddd);
+        this.setTelefone(telefone);
+        this.setEmail(email);
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf == null) {
+            throw new IllegalArgumentException("O campo 'cpf' não pode ser nulo.");
+        }
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null) {
+            throw new IllegalArgumentException("O campo 'nome' não pode ser nulo.");
+        }
+        this.nome = nome;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        if (ddd == null) {
+            throw new IllegalArgumentException("O campo 'ddd' não pode ser nulo.");
+        }
+        this.ddd = ddd;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        if (telefone == null) {
+            throw new IllegalArgumentException("O campo 'telefone' não pode ser nulo.");
+        }
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email == null) {
+            throw new IllegalArgumentException("O campo 'email' não pode ser nulo.");
+        }
+        this.email = email;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
