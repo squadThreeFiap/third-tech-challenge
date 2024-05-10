f5e3d83b71715df715e49041c706caf1c1c51ecd
Barreto <v.barreto.and@gmail.com>
2024-05-10 00:40:38 -0300

    add domain reservation class

---
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/application/entities/Reserva.java
@@ -0,0 +1,130 @@
+package br.com.fiap.squad3.restaurantfinder.application.entities;
+
+import br.com.fiap.squad3.restaurantfinder.application.entities.enums.StatusReserva;
+
+import java.time.LocalDateTime;
+
+public class Reserva {
+    private final int QUANTIDADE_PESSOAS_MINIMA = 1;
+
+    private Long id;
+    private Long idUsuario;
+    private Long idRestaurante;
+    private Integer quantidadePessoas;
+    private LocalDateTime dataHoraInicio;
+    private LocalDateTime dataHoraFim;
+    private StatusReserva status;
+
+    public Reserva(
+            Long idUsuario,
+            Long idRestaurante,
+            Integer quantidadePessoas,
+            LocalDateTime dataHoraInicio,
+            LocalDateTime dataHoraFim,
+            StatusReserva status
+    ) {
+        this.idUsuario = idUsuario;
+        this.idRestaurante = idRestaurante;
+        this.quantidadePessoas = quantidadePessoas;
+        this.dataHoraInicio = dataHoraInicio;
+        this.dataHoraFim = dataHoraFim;
+        this.status = status;
+    }
+
+    public Reserva(
+            Long id,
+            Long idUsuario,
+            Long idRestaurante,
+            Integer quantidadePessoas,
+            LocalDateTime dataHoraInicio,
+            LocalDateTime dataHoraFim,
+            StatusReserva status
+    ) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.idRestaurante = idRestaurante;
+        this.quantidadePessoas = quantidadePessoas;
+        this.dataHoraInicio = dataHoraInicio;
+        this.dataHoraFim = dataHoraFim;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        if (idUsuario == null || idUsuario <= 0) {
+            throw new IllegalArgumentException("Id de usuário inválido.");
+        }
+
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(Long idRestaurante) {
+        if (idRestaurante == null || idRestaurante <= 0) {
+            throw new IllegalArgumentException("Id de restaurante inválido.");
+        }
+
+        this.idRestaurante = idRestaurante;
+    }
+
+    public Integer getQuantidadePessoas() {
+        return quantidadePessoas;
+    }
+
+    public void setQuantidadePessoas(Integer quantidadePessoas) {
+        if (quantidadePessoas == null || quantidadePessoas < QUANTIDADE_PESSOAS_MINIMA) {
+            throw new IllegalArgumentException(
+                    "Quantidade mínima de pessoas deve ser pelo menos " +
+                            QUANTIDADE_PESSOAS_MINIMA + "."
+            );
+        }
+
+        this.quantidadePessoas = quantidadePessoas;
+    }
+
+    public LocalDateTime getDataHoraInicio() {
+        return dataHoraInicio;
+    }
+
+    public void setDataHoraInicio(LocalDateTime dataHoraInicio) {
+        if (dataHoraInicio.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Data de início da reserva não pode estar no passado.");
+        }
+
+        this.dataHoraInicio = dataHoraInicio;
+    }
+
+    public LocalDateTime getDataHoraFim() {
+        return dataHoraFim;
+    }
+
+    public void setDataHoraFim(LocalDateTime dataHoraFim) {
+        if (dataHoraFim.isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Data de fim da reserva não pode estar no passado.");
+        }
+
+        this.dataHoraFim = dataHoraFim;
+    }
+
+    public StatusReserva getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusReserva status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/br/com/fiap/squad3/restaurantfinder/application/entities/ReservaTest.java
@@ -0,0 +1,109 @@
+package br.com.fiap.squad3.restaurantfinder.application.entities;
+
+import br.com.fiap.squad3.restaurantfinder.application.entities.enums.StatusReserva;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ReservaTest {
+
+    private Reserva reserva;
+
+    @BeforeEach
+    void setUp() {
+        reserva = new Reserva(
+                1L,
+                2L,
+                3,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                StatusReserva.AGUARDANDO
+        );
+    }
+
+    @Test
+    void testSetIdUsuario_IdValido() {
+        Long idUsuarioEsperado = 1L;
+
+        Reserva reserva = new Reserva(
+                idUsuarioEsperado,
+                2L,
+                3,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                StatusReserva.AGUARDANDO
+        );
+
+        assertEquals(idUsuarioEsperado, reserva.getIdUsuario());
+    }
+
+    @Test
+    void testSetIdUsuario_IdInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdUsuario(null));
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdUsuario(0L));
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdUsuario(-1L));
+    }
+
+    @Test
+    void testSetIdRestaurante_IdValido() {
+        Long idRestauranteEsperado = 20L;
+
+        reserva.setIdRestaurante(idRestauranteEsperado);
+
+        assertEquals(idRestauranteEsperado, reserva.getIdRestaurante());
+    }
+
+    @Test
+    void testSetIdRestaurante_IdInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdRestaurante(null));
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdRestaurante(0L));
+        assertThrows(IllegalArgumentException.class, () -> reserva.setIdRestaurante(-13L));
+    }
+
+    @Test
+    void testSetQuantidadePessoas_ValorValido() {
+        Integer quantidadePessoasEsperado = 5;
+
+        reserva.setQuantidadePessoas(quantidadePessoasEsperado);
+
+        assertEquals(quantidadePessoasEsperado, reserva.getQuantidadePessoas());
+    }
+
+    @Test
+    void testSetQuantidadePessoas_ValorInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> reserva.setQuantidadePessoas(0));
+        assertThrows(IllegalArgumentException.class, () -> reserva.setQuantidadePessoas(-7));
+    }
+
+    @Test
+    void testSetDataHoraInicio_ValorValido() {
+        LocalDateTime dataHoraInicioEsperada = LocalDateTime.now().plusHours(1);
+
+        reserva.setDataHoraInicio(dataHoraInicioEsperada);
+
+        assertEquals(dataHoraInicioEsperada, reserva.getDataHoraInicio());
+    }
+
+    @Test
+    void testSetDataHoraInicio_ValorInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> reserva.setDataHoraInicio(LocalDateTime.now().minusHours(1)));
+    }
+
+    @Test
+    void testSetDataHoraFim_ValorValido() {
+        LocalDateTime dataHoraFimEsperada = LocalDateTime.now().plusHours(2);
+
+        reserva.setDataHoraFim(dataHoraFimEsperada);
+
+        assertEquals(dataHoraFimEsperada, reserva.getDataHoraFim());
+    }
+
+    @Test
+    void testSetDataHoraFim_ValorInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> reserva.setDataHoraFim(LocalDateTime.now().minusHours(1)));
+    }
+}
