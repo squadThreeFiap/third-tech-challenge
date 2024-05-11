f468b723526c7979e2fad4bdbf1064fe323ba62e
Barreto <v.barreto.and@gmail.com>
2024-05-11 09:52:48 -0300

    add spring beans configuration for reservation components

---
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/configuration/beans/ReservaConfig.java
@@ -0,0 +1,51 @@
+package br.com.fiap.squad3.restaurantfinder.external.restapi.configuration.beans;
+
+import br.com.fiap.squad3.restaurantfinder.application.gateways.ReservaGateway;
+import br.com.fiap.squad3.restaurantfinder.application.gateways.RestauranteGateway;
+import br.com.fiap.squad3.restaurantfinder.application.gateways.UsuarioGateway;
+import br.com.fiap.squad3.restaurantfinder.application.usecases.ReservaUseCase;
+import br.com.fiap.squad3.restaurantfinder.external.jpa.repository.ReservaRepository;
+import br.com.fiap.squad3.restaurantfinder.external.jpa.repository.RestauranteRepository;
+import br.com.fiap.squad3.restaurantfinder.external.jpa.repository.UsuarioRepository;
+import br.com.fiap.squad3.restaurantfinder.interfaceadapters.converters.api.ReservaDtoConverter;
+import br.com.fiap.squad3.restaurantfinder.interfaceadapters.converters.db.ReservaEntityConverter;
+import br.com.fiap.squad3.restaurantfinder.interfaceadapters.repositorygateways.ReservaRepositoryGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReservaConfig {
+    @Bean
+    ReservaUseCase reservaUseCase(
+            UsuarioGateway usuarioGateway,
+            RestauranteGateway restauranteGateway,
+            ReservaGateway reservaGateway
+    ) {
+        return new ReservaUseCase(usuarioGateway, restauranteGateway, reservaGateway);
+    }
+
+    @Bean
+    ReservaGateway reservaGateway(
+            ReservaRepository reservaRepository,
+            ReservaEntityConverter reservaEntityConverter,
+            UsuarioRepository usuarioRepository,
+            RestauranteRepository restauranteRepository
+    ) {
+        return new ReservaRepositoryGateway(
+                reservaRepository,
+                reservaEntityConverter,
+                usuarioRepository,
+                restauranteRepository
+        );
+    }
+
+    @Bean
+    ReservaEntityConverter reservaEntityConverter() {
+        return new ReservaEntityConverter();
+    }
+
+    @Bean
+    ReservaDtoConverter reservaDtoConverter() {
+        return new ReservaDtoConverter();
+    }
+}
