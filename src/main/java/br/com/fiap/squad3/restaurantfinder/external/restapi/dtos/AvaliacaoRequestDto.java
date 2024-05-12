a622f11793a17170b314b368f6e88aaa447c5d0d
Barreto <v.barreto.and@gmail.com>
2024-05-12 10:09:19 -0300

    add endpoint to create reservation ratings

--- src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/dtos/AvaliacaoRequestDto.java
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/dtos/AvaliacaoRequestDto.java
@@ -1,20 +1,18 @@
 package br.com.fiap.squad3.restaurantfinder.external.restapi.dtos;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record AvaliacaoDto(
-        @Schema(hidden = true)
-        Long id,
-        String nome,
-        String comentario,
+public record AvaliacaoRequestDto(
+        @NotNull
+        Long reservaId,
 
         @NotNull
         @Min(1)
         @Max(5)
-        Integer nota,
-        Long restauranteId
+        short nota,
+
+        String comentario
 ) {
 }
---
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/dtos/AvaliacaoResponseDto.java
@@ -0,0 +1,6 @@
+package br.com.fiap.squad3.restaurantfinder.external.restapi.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AvaliacaoResponseDto(@Schema(hidden = true) Long id) {
+}
