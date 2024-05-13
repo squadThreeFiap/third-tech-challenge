adf68b64c033797463acf78c49f73cc712a344cf
Barreto <v.barreto.and@gmail.com>
2024-05-12 21:31:46 -0300

    add endpoint to visualize detailed reservations of a restaurant

--- src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/controllers/RestauranteController.java
+++ src/main/java/br/com/fiap/squad3/restaurantfinder/external/restapi/controllers/RestauranteController.java
@@ -1,31 +1,42 @@
 package br.com.fiap.squad3.restaurantfinder.external.restapi.controllers;
 
+import br.com.fiap.squad3.restaurantfinder.application.entities.ReservaDetalhada;
 import br.com.fiap.squad3.restaurantfinder.application.entities.Restaurante;
 import br.com.fiap.squad3.restaurantfinder.application.usecases.CadastroRestauranteUseCase;
-import br.com.fiap.squad3.restaurantfinder.interfaceadapters.converters.api.RestauranteDtoConverter;
+import br.com.fiap.squad3.restaurantfinder.application.usecases.GerenciamentoRestauranteUseCase;
+import br.com.fiap.squad3.restaurantfinder.external.restapi.dtos.DataWrapperDto;
+import br.com.fiap.squad3.restaurantfinder.external.restapi.dtos.ReservaDetalhadaResponseDto;
 import br.com.fiap.squad3.restaurantfinder.external.restapi.dtos.RestauranteRequestDto;
 import br.com.fiap.squad3.restaurantfinder.external.restapi.dtos.RestauranteResponseDto;
+import br.com.fiap.squad3.restaurantfinder.interfaceadapters.converters.api.ReservaDtoConverter;
+import br.com.fiap.squad3.restaurantfinder.interfaceadapters.converters.api.RestauranteDtoConverter;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/restaurante")
 public class RestauranteController {
     private final CadastroRestauranteUseCase cadastroRestauranteUseCase;
+    private final GerenciamentoRestauranteUseCase gerenciamentoRestauranteUseCase;
     private final RestauranteDtoConverter restauranteDtoConverter;
+    private final ReservaDtoConverter reservaDtoConverter;
+
 
     public RestauranteController(
             CadastroRestauranteUseCase cadastroRestauranteUseCase,
-            RestauranteDtoConverter restauranteDtoConverter
+            GerenciamentoRestauranteUseCase gerenciamentoRestauranteUseCase,
+            RestauranteDtoConverter restauranteDtoConverter,
+            ReservaDtoConverter reservaDtoConverter
     ) {
         this.cadastroRestauranteUseCase = cadastroRestauranteUseCase;
+        this.gerenciamentoRestauranteUseCase = gerenciamentoRestauranteUseCase;
         this.restauranteDtoConverter = restauranteDtoConverter;
+        this.reservaDtoConverter = reservaDtoConverter;
     }
 
     @PostMapping()
@@ -36,6 +47,17 @@ public class RestauranteController {
         RestauranteResponseDto restauranteCadastradoResponse = restauranteDtoConverter.toResponse(restauranteCadastrado);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(restauranteCadastradoResponse);
+    }
+
+    @GetMapping("/{idRestaurante}/reservas")
+    public ResponseEntity<DataWrapperDto> findReservationsByRestaurantId(@PathVariable Long idRestaurante) {
+        List<ReservaDetalhada> reservasDoRestaurante = gerenciamentoRestauranteUseCase.visualizar(idRestaurante);
+
+        List<ReservaDetalhadaResponseDto> reservasDoRestauranteResponse = reservasDoRestaurante.stream()
+                .map(reservaDtoConverter::toDetailedResponse)
+                .toList();
+
+        return ResponseEntity.ok(new DataWrapperDto(reservasDoRestauranteResponse));
     }
 
 //    @PutMapping(
