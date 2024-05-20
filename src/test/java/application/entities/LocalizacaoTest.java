791e03db1d874594f6ce3a6704bdbf1e148bbbbf
Barreto <v.barreto.and@gmail.com>
2024-05-20 20:08:24 -0300

    test location with full coverage

--- src/test/java/application/entities/LocalizacaoTest.java
+++ src/test/java/application/entities/LocalizacaoTest.java
@@ -13,7 +13,32 @@ class LocalizacaoTest {
 
     @BeforeEach
     void setUp() {
-        localizacao = new Localizacao("12345678","SP", "São Paulo", "Centro", "Rua Principal", 123);
+        localizacao = new Localizacao("12345678", "SP", "São Paulo", "Centro", "Rua Principal", 123);
+    }
+
+    @Test
+    void testGetters() {
+        String cep = "12345678";
+        String uf = "SP";
+        String cidade = "São Paulo";
+        String bairro = "Centro";
+        String logradouro = "Rua Principal";
+        Integer numero = 123;
+
+        Localizacao localizacao = new Localizacao(cep, uf, cidade, bairro, logradouro, numero);
+
+        assertEquals(cep, localizacao.getCep());
+        assertEquals(uf, localizacao.getUf());
+        assertEquals(cidade, localizacao.getCidade());
+        assertEquals(bairro, localizacao.getBairro());
+        assertEquals(logradouro, localizacao.getLogradouro());
+        assertEquals(numero, localizacao.getNumero());
+    }
+
+    @Test
+    void testValidacaoCep() {
+        assertThrows(IllegalArgumentException.class, () -> localizacao.setCep("10009-100"));
+        assertThrows(IllegalArgumentException.class, () -> localizacao.setCep("123"));
     }
 
     @Test
@@ -21,9 +46,9 @@ class LocalizacaoTest {
         assertThrows(IllegalArgumentException.class, () -> localizacao.setUf(null));
         assertThrows(IllegalArgumentException.class, () -> localizacao.setUf(" "));
         assertThrows(IllegalArgumentException.class, () -> localizacao.setUf("ABC"));
-        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678",null, "Cidade", "Bairro", "Logradouro", 1));
-        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678","", "Cidade", "Bairro", "Logradouro", 1));
-        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678","ABC", "Cidade", "Bairro", "Logradouro", 1));
+        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678", null, "Cidade", "Bairro", "Logradouro", 1));
+        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678", "", "Cidade", "Bairro", "Logradouro", 1));
+        assertThrows(IllegalArgumentException.class, () -> new Localizacao("12345678", "ABC", "Cidade", "Bairro", "Logradouro", 1));
     }
 
     @Test
@@ -45,7 +70,11 @@ class LocalizacaoTest {
     }
 
     @Test
-    void testGetNumero() {
-        assertEquals(123, localizacao.getNumero());
+    void testGetAndSetNumero() {
+        Integer numero = 123;
+
+        localizacao.setNumero(numero);
+
+        assertEquals(numero, localizacao.getNumero());
     }
 }
